6b3e36557930269bea94b4fbdaae9ebc5ff5d9b6
Wang Yao <yaowang274@gmail.com>
2021-02-25 19:15:07 +1300

    update

--- vote/app.py
+++ vote/app.py
@@ -5,7 +5,7 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Birds")
+option_a = os.getenv('OPTION_A', "llllls")
 option_b = os.getenv('OPTION_B', "Cats")
 hostname = socket.gethostname()
 
